88e03084343f36e4e25fccaad24e52a741d17e69
Igor Lishchenko <igor@lishchenko.com>
2021-03-18 15:15:13 +0200

    Comleted

---
+++ Destini-iOS13/Model/Story.swift
@@ -0,0 +1,17 @@
+//
+//  Sotry.swift
+//  Destini-iOS13
+//
+//  Created by Igor Lishchenko on 07.12.2020.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
